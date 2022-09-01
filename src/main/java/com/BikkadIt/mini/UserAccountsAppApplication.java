9f17940cae9b1b22dd521fb8edf55b7c45075435
Akshaylakhe24 <akshaylakhe24@gmail.com>
2022-09-01 22:15:29 +0530

    MIN-3 : Create Spring Boot application with below dependencies

---
+++ src/main/java/com/BikkadIt/mini/UserAccountsAppApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIt.mini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAccountsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAccountsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BikkadIt/mini/UserAccountsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BikkadIt.mini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAccountsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
